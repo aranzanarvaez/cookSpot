34eef407817eafa7fb932a8cf1292950af82f347
ary2601 <aranzadiva@hotmail.com>
2023-03-23 14:53:28 -0600

    final

--- cookspot/src/components/Header.js
+++ cookspot/src/components/Header.js
@@ -4,44 +4,43 @@ import React,{useEffect,useState} from 'react'
 function Header() {
   return (
     <div>
-    <div id="carouselExample" className="carousel slide">
-        <div className="carousel-inner">
+    <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
 
-          <div className="carousel-item text-center vh-100 bg-cover active slide-1">
-            <div className="container h-100 d-flex align-items-center justify-content-center">
-                <div className="row justify-content-center">
-                    <div className="col-lg-8">
-                        <h6 className="fw-bold text-white">BIENVENIDO A COOKSPOT</h6>
-                        <h2 className="display-1 fw-bold text-white">Prepara tus platillos favoritos</h2>
-                        <a href="#menu" className="btn btn-brand">Busca ahora</a>
+          <div class="carousel-item text-center vh-100 bg-cover active slide-1">
+            <div class="container h-100 d-flex align-items-center justify-content-center">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <h6 class="fw-bold text-white">BIENVENIDO A COOKSPOT</h6>
+                        <h2 class="display-1 fw-bold text-white">Prepara tus platillos favoritos</h2>
+                        <a href="#menu" class="btn btn-brand">Busca ahora</a>
                         
                     </div>
                 </div>
             </div>
           </div>
           
-          <div className="carousel-item text-center vh-100 bg-cover slide-2">
-            <div className="container h-100 d-flex align-items-center justify-content-center">
-                <div className="row justify-content-center">
-                    <div className="col-lg-8">
-                        <h6 className="fw-bold text-white">BIENVENIDO A COOKSPOT</h6>
-                        <h2 className="display-1 fw-bold text-white">Compra los ingredientes en tu super más cercano</h2>
-                        <a href="#menu" className="btn btn-brand">Ve las ubicaciones</a>
+          <div class="carousel-item text-center vh-100 bg-cover slide-2">
+            <div class="container h-100 d-flex align-items-center justify-content-center">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <h6 class="fw-bold text-white">BIENVENIDO A COOKSPOT</h6>
+                        <h2 class="display-1 fw-bold text-white">Compra los ingredientes en tu super más cercano</h2>
+                        <a href="./Map" class="btn btn-brand">Ve las ubicaciones</a>
                     </div>
                 </div>
             </div>
           </div>
 
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
         </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
         </button>
-        
       </div>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossOrigin="anonymous"> </script>
   </div>
